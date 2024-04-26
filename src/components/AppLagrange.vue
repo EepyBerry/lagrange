1a42495135479ba9661327fe023504785e11bc8c
EepyBerry <eepyberry@proton.me>
2024-04-26 21:38:15 +0200

    Add skybox with sun & shadows, clean-up code a bit more

--- src/components/AppLagrange.vue
+++ src/components/AppLagrange.vue
@@ -6,6 +6,7 @@
 import { onMounted, ref, type Ref } from 'vue'
 import * as THREE from 'three'
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import Stats from 'three/addons/libs/stats.module.js';
 import * as ThreeUtils from '@/utils/three-utils';
 
 import fbmFragmentShader from '@/assets/glsl/generative_fbm.frag.glsl?raw'
@@ -15,55 +16,43 @@ import defaultVertexShader from '@/assets/glsl/default.vert.glsl?raw'
 const sceneRoot: Ref<any> = ref(null)
 
 // Main THREE objects
-let scene: THREE.Scene
-let camera: THREE.PerspectiveCamera
-let renderer: THREE.WebGLRenderer
-// End main THREE objects
+let $scene: ThreeUtils.SceneElements
 
 onMounted(() => init())
 
 function init() {
   const width = window.innerWidth, height = window.innerHeight
+  $scene = ThreeUtils.createScene(width, height)
 
-  scene = new THREE.Scene();
-  scene.background = new THREE.Color(0x0)
-  scene.background.convertLinearToSRGB()
-  renderer = new THREE.WebGLRenderer( { antialias: true } )
-  camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10)
-  camera.position.z = 3
+  const mesh = ThreeUtils.createPlanetMesh()
+  $scene.scene.add(mesh)
 
-  const geometry = new THREE.IcosahedronGeometry(1, 10)
-/*   const material = new THREE.MeshBasicMaterial({
-    color: 0xffffff,
-    map: ThreeUtils.loadTextureFromUrl('/2k_earth_daymap.jpg', THREE.SRGBColorSpace),
-    toneMapped: false
-  }) */
-  const material = ThreeUtils.createRawShaderMaterial({
-    u_resolution: { value: new THREE.Vector2(width, height)},
-    u_time: { value: 0 }
-  }, defaultVertexShader, fbmFragmentShader)
+  const sun = ThreeUtils.createSun()
+  sun.position.set(0, 5e3, 1e4)
+  $scene.scene.add(sun)
+  //$scene.scene.add(ThreeUtils.createPlanetWireframe(mesh))
 
-  const mesh = new THREE.Mesh(geometry, material)
-  scene.add(mesh)
+  $scene.renderer.setSize( width, height )
+  $scene.renderer.setAnimationLoop((time: number) => renderFrame(mesh, time, stats))
+  sceneRoot.value.appendChild($scene.renderer.domElement)
 
-  renderer.setSize( width, height )
-  renderer.setAnimationLoop((time: number) => renderFrame(mesh, time))
-  sceneRoot.value.appendChild(renderer.domElement)
+  ThreeUtils.createControls($scene.camera, $scene.renderer.domElement)
+  new GUI();
 
-  const controls = ThreeUtils.createControls(camera, renderer.domElement)
-  const gui = new GUI();
-  gui.add(controls, 'zoomToCursor');
+  const stats = new Stats();
+	document.body.appendChild( stats.dom );
 
-  window.addEventListener( 'resize', onWindowResize );
+  window.addEventListener('resize', onWindowResize);
 }
 
-function renderFrame(mesh: THREE.Mesh, time: number) {
-  renderer.render(scene, camera)
+function renderFrame(mesh: THREE.Mesh, time: number, stats: Stats) {
+  stats.update()
+  $scene.renderer.render($scene.scene, $scene.camera)
 }
 
 function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight
-  camera.updateProjectionMatrix()
-  renderer.setSize(window.innerWidth, window.innerHeight)
+  $scene.camera.aspect = window.innerWidth / window.innerHeight
+  $scene.camera.updateProjectionMatrix()
+  $scene.renderer.setSize(window.innerWidth, window.innerHeight)
 }
 </script>
--- src/utils/three-utils.ts
+++ src/utils/three-utils.ts
@@ -1,11 +1,77 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import { TGALoader } from 'three/addons/loaders/TGALoader.js';
 import * as lygia from 'resolve-lygia';
 
+export type SceneElements = {
+  scene: THREE.Scene,
+  renderer: THREE.WebGLRenderer,
+  camera: THREE.PerspectiveCamera
+}
+
+const CUBE_TEXTURE_LOADER = new THREE.CubeTextureLoader()
 const TEXTURE_LOADER = new THREE.TextureLoader()
+const TGA_LOADER = new TGALoader()
 
 // ----------------------------------------------------------------------------------------------------------------------
 // SCENE FUNCTIONS
+
+export function createScene(width: number, height: number): SceneElements
+{
+  // setup scene
+  const scene = new THREE.Scene()
+  const cubemap: THREE.CubeTexture = CUBE_TEXTURE_LOADER.setPath('/skybox/').load([
+    'space_ft.png',
+    'space_bk.png',
+    'space_up.png',
+    'space_dn.png',
+    'space_rt.png',
+    'space_lf.png',
+  ])
+  cubemap.minFilter = THREE.NearestFilter
+  scene.background = cubemap
+
+  // setup renderer
+  const renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } )
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.setSize(width, height)
+  renderer.setClearColor(0xffffff, 0)
+  renderer.setClearAlpha(0)
+  renderer.shadowMap.enabled = true
+  renderer.shadowMap.autoUpdate = true
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+  // setup camera
+  const camera = new THREE.PerspectiveCamera(70, width / height, 0.1, 1e9)
+  camera.position.z = 3
+
+  return { scene, renderer, camera }
+}
+
+export function createSun() {
+  const geometry = new THREE.IcosahedronGeometry(50, 4)
+  const material = new THREE.MeshStandardMaterial( { color: 0xffffff, emissive: 0xffffff } )
+  const sun = new THREE.PointLight(0xfff6e8, 5e8)
+  sun.castShadow = true
+  sun.receiveShadow = false
+  sun.add(new THREE.Mesh(geometry, material))
+  return sun
+}
+
+export function createPlanetMesh()  {
+  const geometry = new THREE.IcosahedronGeometry(1, 12)
+  /* const material = ThreeUtils.createRawShaderMaterial({
+    u_resolution: { value: new THREE.Vector2(width, height)},
+    u_time: { value: 0 }
+  }, defaultVertexShader, fbmFragmentShader) */
+  const defaultTexture = loadTextureFromUrl('/2k_earth_daymap.jpg', THREE.SRGBColorSpace)
+  const material = new THREE.MeshStandardMaterial ({
+    map: defaultTexture,
+  })
+  const mesh = new THREE.Mesh(geometry, material)
+  mesh.receiveShadow = true
+  return mesh
+}
 
 export function createControls(camera: THREE.Camera, canvas: HTMLCanvasElement): OrbitControls {
   const controls = new OrbitControls( camera, canvas );
@@ -35,12 +101,17 @@ export function loadTextureFromData(data: string) {
  * Loads a texture from a local URL (for pre-stored textures)
  * @param data the image URL
  */
-export function loadTextureFromUrl(url: string, colorSpace:  THREE.ColorSpace) {
-  const texture = TEXTURE_LOADER.load('/2k_earth_daymap.jpg'); 
-  texture.colorSpace = colorSpace
+export function loadTextureFromUrl(url: string, colorSpace: THREE.ColorSpace) {
+  let texture
+  if (url.endsWith('.tga')) {
+    texture = TGA_LOADER.load(url); 
+    texture.colorSpace = colorSpace
+  } else {
+    texture = TEXTURE_LOADER.load(url, undefined, undefined, (err) => console.error(err)); 
+    texture.colorSpace = colorSpace
+  }
   return texture
 }
-
 
 // ----------------------------------------------------------------------------------------------------------------------
 // SHADER FUNCTIONS
@@ -52,18 +123,26 @@ export function loadTextureFromUrl(url: string, colorSpace:  THREE.ColorSpace) {
  * @param fragmentShader GLSL fragment shader
  * @returns the RawShaderMaterial instance
  */
-export function createRawShaderMaterial(uniforms: { [uniform: string]: THREE.IUniform<any>; }, vertexShader: string, fragmentShader: string) {
-  return new THREE.RawShaderMaterial({ uniforms, vertexShader: lygia.resolveLygia(vertexShader), fragmentShader: lygia.resolveLygia(fragmentShader), glslVersion: THREE.GLSL3 })
+export function createRawShaderMaterial(
+  uniforms: { [uniform: string]: THREE.IUniform<any>; },
+  vertexShader: string,
+  fragmentShader: string
+) {
+  return new THREE.RawShaderMaterial({
+    uniforms,
+    vertexShader: lygia.resolveLygia(vertexShader),
+    fragmentShader: lygia.resolveLygia(fragmentShader), glslVersion: THREE.GLSL3
+  })
 }
 
 // ----------------------------------------------------------------------------------------------------------------------
 // DEBUG FUNCTIONS
 
-export function createWireframe(mesh: THREE.Mesh): THREE.LineSegments {
+export function createPlanetWireframe(mesh: THREE.Mesh): THREE.LineSegments {
   const wireframe = new THREE.WireframeGeometry(mesh.geometry)
   const line = new THREE.LineSegments( wireframe );
   const mat = line.material as THREE.Material
-  mat.depthTest = false
+  mat.depthTest = true
   mat.opacity = 0.25
   mat.transparent = true
   return line
