425fed49673882a5371726e0a900f8e22ff84c28
EepyBerry <eepyberry@proton.me>
2024-08-12 13:46:49 +0200

    Start trying stuff out with Voronoi

--- src/assets/glsl/functions/voronoise.func.glsl
+++ src/assets/glsl/functions/voronoise.func.glsl
@@ -25,7 +25,26 @@ float pcurve( float x, float a, float b ){
     return k * pow( x, a ) * pow( 1.0-x, b );
 }
 
-// voronoi implementation largely referenced from https://www.shadertoy.com/view/MslGD8
+// Voronoi implementation largely referenced from https://www.shadertoy.com/view/MslGD8
+// ---
+// The MIT License
+// Copyright © 2013 Inigo Quilez
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial
+// portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// https://www.youtube.com/c/InigoQuilez
+// https://iquilezles.org/
 vec2 voronoi3( in vec3 x, in float time )
 {
     vec3 n = floor(x);
