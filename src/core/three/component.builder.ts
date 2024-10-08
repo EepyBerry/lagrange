8e33a339d708267e5d77d5a948578484f7182179
EepyBerry <eepyberry@proton.me>
2024-10-08 22:19:42 +0200

    Update CSM, remove old silent param

--- src/core/three/component.builder.ts
+++ src/core/three/component.builder.ts
@@ -106,7 +106,6 @@ export function createCustomShaderMaterialComponent<T extends MaterialConstructo
     vertexShader,
     fragmentShader: fragmentShader ? resolveImports(fragmentShader) : undefined,
     uniforms,
-    silent: true,
   })
   return mat
 }
@@ -119,7 +118,7 @@ export function createShaderMaterialComponent(
   return new THREE.ShaderMaterial({
     vertexShader,
     fragmentShader: fragmentShader ? resolveImports(fragmentShader) : undefined,
-    uniforms
+    uniforms,
   })
 }
 
