41ab299f538c709c9b5ac7f85bbf0a1d352d1574
EepyBerry <eepyberry@proton.me>
2025-10-03 15:16:43 +0200

    Add comments + minor formatting

--- src/core/helpers/export.helper.ts
+++ src/core/helpers/export.helper.ts
@@ -5,20 +5,29 @@ import { setObjectValue } from '../utils/utils'
 
 const GLTF_EXPORTER = new GLTFExporter()
 
+/**
+ * Exports a list of meshes to a glTF file, which is then automatically downloaded to the user's device
+ * @param meshes meshes to export, usually the full planet w/o atmosphere
+ * @param filename name of the glTF file (without .gltf extension)
+ */
 export function exportMeshesToGLTF(meshes: Mesh[], filename: string): void {
   GLTF_EXPORTER.parse(
     meshes,
     (data) => {
       console.info('<Lagrange> Exported meshes to GLTF successfully!')
       const gltfString = JSON.stringify(patchGLTFProperties(data as Record<string, unknown>))
-
       saveAs(new Blob([gltfString]), `${filename}.gltf`)
     },
     (err) => console.error(err),
   )
 }
 
+/**
+ * Patches certain values in the glTF file to work around three.js limitations
+ * @param gltf the gltf object to patch
+ * @returns the patched object
+ */
 function patchGLTFProperties(gltf: Record<string, unknown>): Record<string, unknown> {
-  setObjectValue(gltf, 'materials[0].emissiveFactor', [1,1,1])
+  setObjectValue(gltf, 'materials[0].emissiveFactor', [1,1,1]) // emissiveFactor; isn't set when emissiveMap and/or emissiveNode are already set
   return gltf
 }
